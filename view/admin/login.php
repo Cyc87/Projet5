19c8becd8e6c7ca415092d9750db0be058b317d8
Cyc87 <chenalcyril87@gmail.com>
2019-12-02 15:45:47 +0100

    modification login

--- view/admin/login.php
+++ view/admin/login.php
@@ -4,7 +4,9 @@
 
 <section>
     <div class="container" style="margin-top:150px;">
+
         <form action="index.php?action=login" method="post">
+
             <div class="card text-center" style="width: 18rem;">
                 <img src="public/images/logo.png" class="card-img-top" alt="...">
                 <ul class="list-group list-group-flush">
@@ -18,20 +20,21 @@
                     </div>
                 </ul>
                 <div class="card-body">
-                    <button type="submit">Connection</button>
+                    <button class="btn btn-info" type="submit">Connection</button><br><br>
+                    <a class="btn btn-dark" href="index.php?action=home" type="submit">Retour à l'accueil</a>
                 </div>
-            </div>   
+            </div>
         </form>
         <?php
-            if(isset($_SESSION['message'])){
-        ?>
-        <div class="alert alert-<?=$_SESSION['msg_type'] ?>" style="height:50px; left:50px;">
-        <?php
-            echo $_SESSION['message'];
-            unset($_SESSION['message']);
+        if (isset($_SESSION['message'])) {
+            ?>
+            <div class="alert alert-<?= $_SESSION['msg_type'] ?>" style="height:50px; left:50px;">
+            <?php
+                echo $_SESSION['message'];
+                unset($_SESSION['message']);
             }
-        ?>
-    </div>        
+            ?>
+            </div>
 </section>
 
 <?php $content = ob_get_clean(); ?>
